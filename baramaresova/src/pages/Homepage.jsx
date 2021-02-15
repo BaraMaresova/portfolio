67ef8e6026214ece6bd5596c600cb889ddc39160
Bara Maresova <bbmaresova@gmail.com>
2021-02-15 14:28:44 +0100

    background added, picture squared

--- baramaresova/src/pages/Homepage.jsx
+++ baramaresova/src/pages/Homepage.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Header } from '../components/Header/Header.jsx';
 import { Timeline } from '../components/Timeline/Timeline.jsx';
 import { Skills } from '../components/Skills/Skills.jsx';
+import './homepage.css';
 
 export const Homepage = () => {
   return (
