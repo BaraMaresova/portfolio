393b1fbb1f1ec6566ce520ba5c73fc0893435797
Bara Maresova <bbmaresova@gmail.com>
2021-02-19 21:05:12 +0100

    znovupřidání funkce na zarovnání do kruhu

--- baramaresova/src/components/Skills/Skills.jsx
+++ baramaresova/src/components/Skills/Skills.jsx
@@ -1,8 +1,36 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { SkillItem } from './SkillItem/SkillItem.jsx';
+import $ from 'jquery';
 import './skills.scss';
 
 export const Skills = () => {
+  useEffect(() => {
+    const circleType = 1,
+      radius = '12em', //distance from center
+      start = -90, //shift start from 0
+      $elements = $('.skillItem'),
+      numberOfElements =
+        circleType === 1 ? $elements.length : $elements.length - 1, //adj for even distro of elements when not full circle
+      slice = (360 * circleType) / numberOfElements;
+
+    $elements.each(function (i) {
+      const $self = $(this),
+        rotate = slice * i + start,
+        rotateReverse = rotate * -1;
+
+      $self.css({
+        transform:
+          'rotate(' +
+          rotate +
+          'deg) translate(' +
+          radius +
+          ') rotate(' +
+          rotateReverse +
+          'deg)',
+      });
+    });
+  }, []);
+
   return (
     <div className="skills">
       <SkillItem skillName="HTML" details="Detaily netřeba" progress="90%" />
@@ -14,6 +42,8 @@ export const Skills = () => {
       />
       <SkillItem skillName="React" details="Detaily netřeba" progress="70%" />
       <SkillItem skillName="Figma" details="Ta mě baví" progress="80%" />
+      <SkillItem skillName="jQuery" details="Ta mě baví" progress="80%" />
+      <SkillItem skillName="SASS" details="Ta mě baví" progress="80%" />
     </div>
   );
 };
