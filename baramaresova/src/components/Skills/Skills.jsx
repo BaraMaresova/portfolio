9bde33be4a04b4e58d08e8595c47a6cabd1d6377
Bara Maresova <bbmaresova@gmail.com>
2021-03-17 20:30:01 +0100

    json na skills

--- baramaresova/src/components/Skills/Skills.jsx
+++ baramaresova/src/components/Skills/Skills.jsx
@@ -6,23 +6,7 @@ import './skills.scss';
 export const Skills = () => {
   return (
     <section id="skills" className="skills_section">
-      <div className="skills">
-        <SkillItem
-          skillName="HTML"
-          details="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
-          progress="90"
-        />
-        <SkillItem skillName="CSS" details="Detaily netřeba" progress="80" />
-        <SkillItem
-          skillName="Javascript"
-          details="Detaily netřeba"
-          progress="50"
-        />
-        <SkillItem skillName="React" details="Detaily netřeba" progress="70" />
-        <SkillItem skillName="Figma" details="Ta mě baví" progress="40" />
-        <SkillItem skillName="jQuery" details="Ta mě baví" progress="80" />
-        <SkillItem skillName="SASS" details="Ta mě baví" progress="80" />
-      </div>
+      <div className="skills"></div>
       <div className="skillsTitle">
         <h2>Dovednosti</h2>
       </div>
--- baramaresova/src/components/Statement/Statement.jsx
+++ baramaresova/src/components/Statement/Statement.jsx
@@ -5,7 +5,7 @@ export const Statement = () => {
   return (
     <div className="statement_div">
       <p className="statement_text">
-        My dream is to start transferring experiences from sales into successful
+        My goal is to start transferring experiences from sales into successful
         websites.
       </p>
     </div>
