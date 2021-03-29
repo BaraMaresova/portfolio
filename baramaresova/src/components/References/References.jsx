2b427d253f74a5745f8708a99455049d40ba8d0a
Bara Maresova <bbmaresova@gmail.com>
2021-03-29 18:21:18 +0200

    resized video

--- baramaresova/src/components/References/References.jsx
+++ baramaresova/src/components/References/References.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { ReferenceItem } from '../References/ReferenceItem/ReferenceItem';
-import moninvo_gif from '../../images/moninvo_gif.gif';
+import moninvo_gif from '../../images/moninvo_resized.gif';
 import moninvo from '../../images/moninvo.png';
 import consultVision from '../../images/Cvicon.svg';
 import cvplaceholder from '../../images/ConsultVision_placeholder.jpg';
@@ -17,7 +17,7 @@ export const References = () => {
           ref_name={'Moninvo'}
           ref_href="https://www.moninvo.cz/#/"
           ref_details="Aplikace na monitorování nákladů, umožňuje nahrání faktur ve formátu pdf, přečte data z faktury a zobrazí je v grafu a tabulce. Projekt získal ocenění Projekt s nejvyšší technickou úrovní během studia Digitální akademie - Web od Czechitas."
-          r_line_length="referenceLine referenceLine-long" 
+          r_line_length="referenceLine referenceLine-long"
         />
         <ReferenceItem
           ref_src={consultVision}
