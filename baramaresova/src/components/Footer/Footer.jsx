7dbbfaefb4874079bea8faf9a8d95aa2c179525b
Bara Maresova <bbmaresova@gmail.com>
2021-07-14 21:47:18 +0200

    email

--- baramaresova/src/components/Footer/Footer.jsx
+++ baramaresova/src/components/Footer/Footer.jsx
@@ -8,33 +8,33 @@ import './footer.scss';
 export const Footer = () => {
   return (
     <footer className="footer">
-        <div
-          className="footer_contact"
-          onClick={() => {
-            window.open('mailto:bbmaresova@gmail.com');
-          }}
-        >
-          <img src={email} alt="ikona zprávy" className="footer_icon" />
-          <span className="footer_span">Email</span>
-        </div>
-        <div
-          className="footer_contact"
-          onClick={() => {
-            window.open('https://github.com/BaraMaresova');
-          }}
-        >
-          <img src={github} alt="ikona githubu" className="footer_icon" />
-          <span className="footer_span">GitHub</span>
-        </div>
-        <div
-          className="footer_contact"
-          onClick={() => {
-            window.open('https://www.linkedin.com/in/bara-maresova');
-          }}
-        >
-          <img src={linkedin} alt="ikona linkedinu" className="footer_icon" />
-          <span className="footer_span">LinkedIn</span>
-        </div>
+      <div
+        className="footer_contact"
+        onClick={() => {
+          window.open('mailto:bara@spacebarkeepers.cz');
+        }}
+      >
+        <img src={email} alt="ikona zprávy" className="footer_icon" />
+        <span className="footer_span">Email</span>
+      </div>
+      <div
+        className="footer_contact"
+        onClick={() => {
+          window.open('https://github.com/BaraMaresova');
+        }}
+      >
+        <img src={github} alt="ikona githubu" className="footer_icon" />
+        <span className="footer_span">GitHub</span>
+      </div>
+      <div
+        className="footer_contact"
+        onClick={() => {
+          window.open('https://www.linkedin.com/in/bara-maresova');
+        }}
+      >
+        <img src={linkedin} alt="ikona linkedinu" className="footer_icon" />
+        <span className="footer_span">LinkedIn</span>
+      </div>
     </footer>
   );
 };
